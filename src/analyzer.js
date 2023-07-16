d9adef1ec0092240cfe965e90048f7270f11f5c8
MarFueGit <maricelafuentes100@gmail.com>
2023-07-15 18:12:41 -0600

    fix: se agrego logica del analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,42 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+    const palabras = text.trim().split(" ").length;
+    return palabras;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const caracteres = text.split("").length;
+    return caracteres;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    // Antes de contar los caracteres debemos limpiar la variable text, borrando espacios y signos de puntuacion.
+    const textoLimpio = text.replace(/\W/g, "");
+    const caracteresSinEspacio = textoLimpio.split("").length;
+    return caracteresSinEspacio;
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    // paso 1: Separamos text en un array de caracteres
+    const caracteres = text.split("");
+    // Paso 2: Con ayuda de filter(), filtramos los caracteres que hagan match con la expresion regular de numeros (/(\d+)/g)
+    const numeros = caracteres.filter((caracter) =>
+      caracter.match(/(\d+)/g)
+    ).length;
+    return numeros;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const caracteres = text.split("");
+    const numeros = caracteres.filter((caracter) => caracter.match(/(\d+)/g));
+    const numerosConvertidos = numeros.map((numero) => Number(numero));
+    const suma = numerosConvertidos.reduce((a, b) => a + b);
+    return suma;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,31 +1,51 @@
 import analyzer from "./analyzer.js";
 
 // Declaramos uns variable boton que hace referencia al boton para limpiar la textarea
-const boton = document.querySelector("resetButton");
+const resetButton = document.getElementById("resetButton");
+
 // Declaramos una variable haciendo referencia a la caja textarea,
 const textarea = document.getElementsByName("text")[0];
 
 // Dclaramos una variable que hace referencia al li de cantidad de caracteres, accdediendo a el
 // por medio de su attibuto data-tesid=chacarcter-count
-const caracteres = document.querySelector('[data-testid="character-count"]');
+const caracteres = document.querySelector('li[data-testid="character-count"]');
 const caracteresSinEspacio = document.querySelector(
-  '[data-testid="character-no-spaces-count"]'
+  'li[data-testid="character-no-spaces-count"]'
 );
-const palabras = document.querySelector('[data-testid="word-count"]');
-const numeros = document.querySelector('[data-testid="number-count"]');
-const sumaTotalDeNumeros = document.querySelector('[data-testid="number-sum"]');
+const palabras = document.querySelector('li[data-testid="word-count"]');
+const numeros = document.querySelector('li[data-testid="number-count"]');
+const sumaTotalDeNumeros = document.querySelector(
+  'li[data-testid="number-sum"]'
+);
 const promedioLongitud = document.querySelector(
-  '[data-testid="word-length-average"]'
+  'li[data-testid="word-length-average"]'
 );
 //Listeners
 resetButton.addEventListener("click", (e) => {
-  console.log(textarea.value);
   e.preventDefault();
   textarea.value = "";
+  palabras.textContent = "Palabras: 0";
+  caracteres.textContent = "Caracteres: 0";
+  caracteresSinEspacio.textContent = "Caracteres sin espacio: 0";
+  numeros.textContent = "Numeros: 0";
 });
 // Listener de textarea, usamos Keyup cuando el usuario escriba y levanta el dedo
-textarea.addEventListener("keyup", (e) => {
-  console.log(textarea.value);
+textarea.addEventListener("keyup", () => {
+  // Seteamos la cantidad de palabras
+  const cantidadPalabras = analyzer.getWordCount(textarea.value);
+  palabras.textContent = "Palabras: " + cantidadPalabras;
 
- 
+  // Seteamos la cantidad de caracteres
+  const cantidadCaracteres = analyzer.getCharacterCount(textarea.value);
+  caracteres.textContent = "Caracteres: " + cantidadCaracteres;
+
+  // Seteamos la cantidad de caracteres sin espacio, ni signo de puntuacion
+  const cantidadCaracteresSinEspacio =
+    analyzer.getCharacterCountExcludingSpaces(textarea.value);
+  caracteresSinEspacio.textContent =
+    "Caracteres sin espacio: " + cantidadCaracteresSinEspacio;
+
+  // Seteamos la cantidad de numeros encontrados en el texto.
+  const cantidadNumeros = analyzer.getNumberCount(textarea.value);
+  numeros.textContent = "Numeros: " + cantidadNumeros;
 });
