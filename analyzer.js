0770398730216494beecd8865815492ee0f95f56
MarFueGit <maricelafuentes100@gmail.com>
2023-07-18 15:24:21 -0600

    Updates

---
+++ analyzer.js
@@ -0,0 +1,57 @@
+const analyzer = {
+  getWordCount: (text) => {
+    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const palabras = text.trim().split(" ").length;
+    return palabras;
+  },
+  getCharacterCount: (text) => {
+    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    const caracteres = text.trim().split("").length;
+    return caracteres;
+  },
+  getCharacterCountExcludingSpaces: (text) => {
+    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    // Antes de contar los caracteres debemos limpiar la variable text, borrando espacios y signos de puntuacion.
+    const textoLimpio = text.replace(/\W/g, "");
+    const caracteresSinEspacio = textoLimpio.split("").length;
+    return caracteresSinEspacio;
+  },
+  getAverageWordLength: (text) => {
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const palabras = text.trim().split(" ");
+    let sumaLongitudes = 0;
+    for (const palabra of palabras) {
+      sumaLongitudes = sumaLongitudes + palabra.length;
+    }
+    const media = sumaLongitudes / palabras.length;
+    return Number(media.toFixed(2));
+  },
+  getNumberCount: (text) => {
+    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    // paso 1: Separamos text en un array de caracteres
+    const palabras = text.trim().split(" ");
+    // Paso 2: Declaramos una variable que usaremos como contador, mientras vamos iterando con un for, validamos
+    //  si es verdadera incrementamos en 1 nuestro contador.
+    let cantidadNumeros = 0;
+    for (const palabra of palabras) {
+      if (!isNaN(palabra)) {
+        cantidadNumeros++;
+      }
+    }
+    return cantidadNumeros;
+  },
+  getNumberSum: (text) => {
+    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const palabras = text.trim().split(" ");
+    // Variable para guardar la suma total de los numeros
+    let sumaNumeros = 0;
+    for (const palabra of palabras) {
+      if (!isNaN(Number(palabra))) {
+        sumaNumeros = sumaNumeros + Number(palabra);
+      }
+    }
+    return sumaNumeros;
+  },
+};
+
+export default analyzer;
---
+++ index.js
@@ -0,0 +1,61 @@
+import analyzer from "./analyzer.js";
+
+// Declaramos uns variable boton que hace referencia al boton para limpiar la textarea
+const resetButton = document.getElementById("resetButton");
+
+// Declaramos una variable haciendo referencia a la caja textarea,
+const textarea = document.getElementsByName("text")[0];
+
+// Dclaramos una variable que hace referencia al li de cantidad de caracteres, accdediendo a el
+// por medio de su attibuto data-tesid=chacarcter-count
+const caracteres = document.querySelector('li[data-testid="character-count"]');
+const caracteresSinEspacio = document.querySelector(
+  'li[data-testid="character-no-spaces-count"]'
+);
+const palabras = document.querySelector('li[data-testid="word-count"]');
+const numeros = document.querySelector('li[data-testid="number-count"]');
+const sumaTotalDeNumeros = document.querySelector(
+  'li[data-testid="number-sum"]'
+);
+const promedioLongitud = document.querySelector(
+  'li[data-testid="word-length-average"]'
+);
+//Listeners
+resetButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  textarea.value = "";
+  palabras.textContent = "Palabras: 0";
+  caracteres.textContent = "Caracteres: 0";
+  caracteresSinEspacio.textContent = "Caracteres sin espacio: 0";
+  numeros.textContent = "Numeros: 0";
+  sumaTotalDeNumeros.textContent = "Suma total de numeros: 0";
+  promedioLongitud.textContent = "Promedio longitud: 0";
+});
+// Listener de textarea, usamos Keyup cuando el usuario escriba y levanta el dedo de la tecla
+textarea.addEventListener("keyup", () => {
+  // Seteamos la cantidad de palabras
+  const cantidadPalabras = analyzer.getWordCount(textarea.value);
+  palabras.textContent = "Palabras: " + cantidadPalabras;
+
+  // Seteamos la cantidad de caracteres
+  const cantidadCaracteres = analyzer.getCharacterCount(textarea.value);
+  caracteres.textContent = "Caracteres: " + cantidadCaracteres;
+
+  // Seteamos la cantidad de caracteres sin espacio, ni signo de puntuacion
+  const cantidadCaracteresSinEspacio =
+    analyzer.getCharacterCountExcludingSpaces(textarea.value);
+  caracteresSinEspacio.textContent =
+    "Caracteres sin espacio: " + cantidadCaracteresSinEspacio;
+
+  // Seteamos la longitud media de palabras encontradas en el texto
+  const mediaLongitud = analyzer.getAverageWordLength(textarea.value)
+  promedioLongitud.textContent = "Promedio longitud: " + mediaLongitud;
+
+  // Seteamos la cantidad de numeros encontrados en el texto.
+  const cantidadNumeros = analyzer.getNumberCount(textarea.value);
+  numeros.textContent = "Numeros: " + cantidadNumeros;
+
+  // Seteamos la suma de los numeros encontrados en el string.
+  const suma = analyzer.getNumberSum(textarea.value);
+  sumaTotalDeNumeros.textContent = "Suma total de numeros: " + suma;
+});
