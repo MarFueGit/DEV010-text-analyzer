262ca04e60f4df167bbc4c95fe94aa39212f74fd
MarFueGit <maricelafuentes100@gmail.com>
2023-07-18 21:28:58 -0600

    se avanzo con los test y refactorize el analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -31,10 +31,10 @@ const analyzer = {
     // paso 1: Separamos text en un array de caracteres
     const palabras = text.trim().split(" ");
     // Paso 2: Declaramos una variable que usaremos como contador, mientras vamos iterando con un for, validamos
-    // cada palabra con la expresion regular, si es verdadera incrementamos en 1 nuestro contador.
+    //  si es verdadera incrementamos en 1 nuestro contador.
     let cantidadNumeros = 0;
-    for (let i = 0; i < palabras.length; i++) {
-      if (palabras[i].match(/^[0-9]\d*(\.\d+)?$/)) {
+    for (const palabra of palabras) {
+      if (!isNaN(palabra)) {
         cantidadNumeros++;
       }
     }
@@ -45,11 +45,9 @@ const analyzer = {
     const palabras = text.trim().split(" ");
     // Variable para guardar la suma total de los numeros
     let sumaNumeros = 0;
-    // Iteramos sobre la array de palabras, si la palabra da verdadero con la expresion regular
-    // convertimos palabra a nomber y lo sumamos.
-    for (let i = 0; i < palabras.length; i++) {
-      if (palabras[i].match(/^[0-9]\d*(\.\d+)?$/)) {
-        sumaNumeros = sumaNumeros + Number(palabras[i]);
+    for (const palabra of palabras) {
+      if (!isNaN(Number(palabra))) {
+        sumaNumeros = sumaNumeros + Number(palabra);
       }
     }
     return sumaNumeros;
