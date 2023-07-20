4f76f34d150a341d297d6baf20764d6593d9f502
MarFueGit <maricelafuentes100@gmail.com>
2023-07-19 19:35:14 -0600

    Updates

--- analyzer.js
+++ analyzer.js
@@ -1,12 +1,20 @@
 const analyzer = {
   getWordCount: (text) => {
+    text = text.trim();
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    const palabras = text.trim().split(" ").length;
+    if (text === "") {
+      // Si text esta vacio retornamos 0
+      return 0;
+    } else if (!/[a-zA-Z]/.test(text)) {
+      // validamos si el texto por lo menos tiene una letra
+      return 0;
+    }
+    const palabras = text.split(" ").length;
     return palabras;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-    const caracteres = text.trim().split("").length;
+    const caracteres = text.split("").length;
     return caracteres;
   },
   getCharacterCountExcludingSpaces: (text) => {
@@ -17,8 +25,16 @@ const analyzer = {
     return caracteresSinEspacio;
   },
   getAverageWordLength: (text) => {
+    text = text.trim();
+    if (text === "") {
+      // Si text esta vacio retornamos 0
+      return 0;
+    } else if (!/[a-zA-Z]/.test(text)) {
+      // validamos si el texto por lo menos tiene una letra
+      return 0;
+    }
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    const palabras = text.trim().split(" ");
+    const palabras = text.split(" ");
     let sumaLongitudes = 0;
     for (let i = 0; i < palabras.length; i++) {
       sumaLongitudes = sumaLongitudes + palabras[i].length;
@@ -27,26 +43,37 @@ const analyzer = {
     return Number(media.toFixed(2));
   },
   getNumberCount: (text) => {
+    text = text.trim();
+    if (text === "") {
+      // Si text esta vacio retornamos 0
+      return 0;
+    }
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
     // paso 1: Separamos text en un array de caracteres
-    const palabras = text.trim().split(" ");
+    const palabras = text.split(" ");
     // Paso 2: Declaramos una variable que usaremos como contador, mientras vamos iterando con un for, validamos
-    //  si es verdadera incrementamos en 1 nuestro contador.
     let cantidadNumeros = 0;
     for (const palabra of palabras) {
       if (!isNaN(Number(palabra))) {
-        cantidadNumeros++;
+        // Convertimos palabra a numero para ver si retorna NaN
+        cantidadNumeros++; //  si es verdadera incrementamos en 1 nuestro contador.
       }
     }
     return cantidadNumeros;
   },
   getNumberSum: (text) => {
+    text = text.trim();
+    if (text === "") {
+      // Si text esta vacio retornamos 0
+      return 0;
+    }
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    const palabras = text.trim().split(" ");
+    const palabras = text.split(" ");
     // Variable para guardar la suma total de los numeros
     let sumaNumeros = 0;
     for (const palabra of palabras) {
       if (!isNaN(Number(palabra))) {
+        // Convertimos palabra a numero para ver si retorna NaN
         sumaNumeros = sumaNumeros + Number(palabra);
       }
     }
